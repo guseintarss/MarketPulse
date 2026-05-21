0c135399f053887ecaae4e9a4a3e0e194e25951d
guseintarss <kingsmanedvard@gmail.com>
2026-05-22 01:08:18 +0300

    Create user model

--- backend/app/core/models/__init__.py
+++ backend/app/core/models/__init__.py
@@ -1,10 +1,12 @@
 __all__ = (
     "Base",
     "Product",
+    "User",
     "DatabaseHelper",
     "db_helper",
 )
 
 from .base import Base
 from .product import Product
+from .user import User
 from .db_helper import DatabaseHelper, db_helper
---
+++ backend/app/core/models/user.py
@@ -0,0 +1,7 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+from .base import Base
+
+
+class User(Base):
+    username: Mapped[str] = mapped_column(String(55), unique=True, nullable=True)
